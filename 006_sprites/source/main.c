45938e053ed1e416f4b4f2d7ad4afdc80c86c52d
Christianfg99 <chrisfergo99@gmail.com>
2022-02-11 12:28:05 +0100

    006_sprites project change

--- 006_sprites/source/main.c
+++ 006_sprites/source/main.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include "gba_types.h"
 #include "gba_gfx.h"
 #include "gba_mathUtil.h"
@@ -8,8 +10,37 @@
 #include "ball.h"
 #include "paddle.h"
 
-#include "img_gba_pong_menu.h"
-#include <string.h>
+#include "img/img_gba_testTiles.h"
+
+
+typedef unsigned char      u8;
+typedef unsigned short     u6;
+typedef unsigned int       u32;
+
+#define MEM_OBJ_PALETTE   ((u16*)(0x05000200))
+void UploadPaletteMem()
+{
+    memcpy(MEM_OBJ_PALETTE, spritePal, spritePalLen);
+
+}
+
+typedef u32 Tile[16];
+typedef Tile TileBlock[256];
+
+#define MEM_VRAM        ((v_u32*)0x06000000)
+#define MEM_TILE        ( (TileBlock*)MEM_VRAM )
+
+void UploadTileMem()
+{
+    memcpy(&MEM_TILE[4][1], spriteTiles, spriteTilesLen);
+}
+
+typedef struct ObjectAttributes {
+    u16 attr0;
+    u16 attr1;
+    u16 attr2;
+    u16 pad;
+} __attribute__((packed, aligned(4))) ObjectAttributes;
 
 /**
  * @brief Comprueba la colision entre a_paddle y a_ball.
@@ -31,10 +62,9 @@ bool check_collisions(struct paddle* a_paddle, struct ball* a_ball)
 
 int main()
 {
-	//set GBA rendering context to MODE 3 Bitmap Rendering
-	REG_DISPCNT = VIDEOMODE_3 | BGMODE_2;
-	//set background image
-	memcpy(SCREENBUFFER, img_gba_pong_menuBitmap, img_gba_pong_menuBitmapLen);
+	//set GBA rendering context to MODE 0 Bitmap Rendering and no background
+	REG_DISPCNT = VIDEOMODE_0;
+	
 
 	//set up GBA registers so VBLANK interrupts are sent
 	register_vblank_isr(); 
@@ -93,9 +123,9 @@ int main()
 		}
 
 		//draw
-		draw_ball(&ball);
-		draw_paddle(&paddle1);
-		draw_paddle(&paddle2);
+		//draw_ball(&ball);
+		//draw_paddle(&paddle1);
+		//draw_paddle(&paddle2);
 	}
 
 	return 0;
