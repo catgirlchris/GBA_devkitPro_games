80bce086ea30214318714bed1bd3ff6152814ba3
Kyoko <47301396+catgirlchris@users.noreply.github.com>
2023-02-13 18:40:28 +0100

    template renaming

---
+++ 000_template_chris/build/main.d
@@ -0,0 +1,16 @@
+main.o: \
+ /mnt/d/proyectos/GBA_devkitPro/010_windows_y_linux_integrados/source/main.c \
+ /opt/devkitpro/libgba/include/gba_base.h \
+ /opt/devkitpro/libgba/include/gba_types.h \
+ /opt/devkitpro/libgba/include/gba_video.h \
+ /opt/devkitpro/libgba/include/gba_base.h \
+ /opt/devkitpro/libgba/include/gba_systemcalls.h \
+ /opt/devkitpro/libgba/include/gba_interrupt.h \
+ /mnt/d/proyectos/GBA_devkitPro/010_windows_y_linux_integrados/build/r6502_portfont_bin.h
+/opt/devkitpro/libgba/include/gba_base.h:
+/opt/devkitpro/libgba/include/gba_types.h:
+/opt/devkitpro/libgba/include/gba_video.h:
+/opt/devkitpro/libgba/include/gba_base.h:
+/opt/devkitpro/libgba/include/gba_systemcalls.h:
+/opt/devkitpro/libgba/include/gba_interrupt.h:
+/mnt/d/proyectos/GBA_devkitPro/010_windows_y_linux_integrados/build/r6502_portfont_bin.h:
---
+++ 000_template_chris/readme.txt
@@ -0,0 +1,22 @@
+
+
+A template taken from devkitpro examples. Below's text is what was written on them.
+
+This was built using Wintermute's GCC toolchain for win32 environment, and
+that can be found here:
+
+http://www.devkit.tk/
+
+or
+
+http://homepage.ntlworld.com/wintermute2002/
+
+Yes, you should be using this, NOT outdated toolchains.
+It is leaner, faster, up to date and uses MingW env.
+
+
+  Code and font by:
+     r6502 (r6502ATphokos.com)
+     2004-04-02
+
+  Happy Coding!
