a5c482cdcd98597204156e0d78abb50bac3481ae
catgirlchris <chrisfergo99@gmail.com>
2025-10-15 20:39:23 +0200

    Missing min and max declarations

--- 006_sprites/include/gba_mathUtil.h
+++ 006_sprites/include/gba_mathUtil.h
@@ -9,5 +9,7 @@ extern s32 gba_generate_random();
 extern s32 gba_generate_random_range(s32 a_min, s32 a_max);
 extern s32 sign(s32 a_val);
 extern s32 abs(s32 a_val);
+extern s32 min(s32 n1, s32 n2);
+extern s32 max(s32 n1, s32 n2);
 
 #endif //__GBA_MATHUTIL_H__
